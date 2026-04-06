6f228972161abc7d408ef20c3dbfa5507c3326d5
Luiz Fernando Piluti <fpiluti@outlook.com>
2026-04-06 15:15:13 -0300

    .

--- WebApiAws.Api/Program.cs
+++ WebApiAws.Api/Program.cs
@@ -4,14 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
+    app.UseSwagger(options => options.OpenApiVersion =
+Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0);
+    app.UseSwaggerUI();
+
 }
 
 app.UseAuthorization();
